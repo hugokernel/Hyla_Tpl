34ce49752c9c9d0ccd20a29162357d99e7c763f5
hugokernel <hugo@digitalspirit.org>
2014-11-07 18:54:49 +0100

    Clean

--- hyla_tpl.class.php
+++ hyla_tpl.class.php
@@ -238,17 +238,17 @@ class Hyla_Tpl {
 
             // Run function on var
             // First, run set var function (&xxx)
-			$data = preg_replace_callback(
-			    '/{&([a-zA-Z_\-0-9]*)\:((\\\\}|\\\\|[^}])*)}/',
-			    function($m) { return $this->setVar($m[1], self::_skipQuote(stripslashes($m[2]))); },
-			    $data
-			);
+            $data = preg_replace_callback(
+                '/{&([a-zA-Z_\-0-9]*)\:((\\\\}|\\\\|[^}])*)}/',
+                function($m) { return $this->setVar($m[1], self::_skipQuote(stripslashes($m[2]))); },
+                $data
+            );
 
-			$data = preg_replace_callback(
-			    '/{([$|!|_|#])(([a-zA-Z_\-0-9]*)\:?((\\\\}|\\\\|[^}])*))}/',
-			    function($m) { return $this->_parseFuncVar($m[2], $m[1]); },
-			    $data
-			);
+            $data = preg_replace_callback(
+                '/{([$|!|_|#])(([a-zA-Z_\-0-9]*)\:?((\\\\}|\\\\|[^}])*))}/',
+                function($m) { return $this->_parseFuncVar($m[2], $m[1]); },
+                $data
+            );
         }
 
         // Get content and add it !
